5d7d2d874bf5810e6cede63b39bcaf9307b7d645
Kristian Williams <kristian.andersonwilliams@gmail.com>
2017-12-14 14:33:55 +0100

    Added documentation.

--- Attributes/CompositeForeignKeyAttribute.cs
+++ Attributes/CompositeForeignKeyAttribute.cs
@@ -1,5 +1,11 @@
 ﻿namespace System.ComponentModel.DataAnnotations.Schema
 {
+    //
+    // Summary:
+    //     Denotes the properties used as foreign keys in a relationship. The annotation may
+    //     be placed on the foreign key property and specify the associated navigation property
+    //     name, or placed on a navigation property and specify the associated foreign key
+    //     name.
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class CompositeForeignKeyAttribute: ForeignKeyAttribute
     {
--- Attributes/CompositeKeyAttribute.cs
+++ Attributes/CompositeKeyAttribute.cs
@@ -1,5 +1,8 @@
 ﻿namespace System.ComponentModel.DataAnnotations.Schema
 {
+    //
+    // Summary:
+    //     Denotes one or more properties that uniquely identify an entity.
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class CompositeKeyAttribute : Attribute
     {
